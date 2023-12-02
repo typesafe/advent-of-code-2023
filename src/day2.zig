05ffb8c0116fa9f720ab1b2c09c26b2d0fba29d0
Gino Heyman <gino.heyman@gmail.com>
2023-12-02 20:05:46 +0100

    feat(day2): part 2

--- src/day2.zig
+++ src/day2.zig
@@ -44,3 +44,38 @@ test "day 2, part 1" {
 
     std.debug.print("solution: {}\n", .{sumOfGameIds});
 }
+
+test "day 2, part 2" {
+    var it = try Input.readLines("src/day2.txt", testing.allocator);
+    defer it.deinit() catch unreachable;
+
+    var totalPower: u32 = 0;
+
+    while (it.next()) |line| {
+        var idAndSets = std.mem.splitScalar(u8, line, ':');
+        _ = idAndSets.next().?;
+        var sets = idAndSets.next().?;
+
+        var setIterator = std.mem.splitScalar(u8, sets, ';');
+
+        var rgb = std.mem.zeroes([3]u32);
+        while (setIterator.next()) |set| {
+            // " b blue, gg green, r red" // or any other order, note the leading space
+            var colorAndCount = std.mem.splitScalar(u8, set[1..set.len], ' ');
+            while (colorAndCount.next()) |count| {
+                const v = try std.fmt.parseInt(u8, count, 10);
+                const c = colorAndCount.next().?[0];
+                switch (c) {
+                    'r' => rgb[0] = if (v > rgb[0]) v else rgb[0],
+                    'g' => rgb[1] = if (v > rgb[1]) v else rgb[1],
+                    'b' => rgb[2] = if (v > rgb[2]) v else rgb[2],
+                    else => unreachable,
+                }
+            }
+        }
+
+        totalPower += rgb[0] * rgb[1] * rgb[2];
+    }
+
+    std.debug.print("solution: {}\n", .{totalPower});
+}
