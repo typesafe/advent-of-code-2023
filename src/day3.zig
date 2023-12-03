e596a0c705b026935349738ae9b9c4de3488a601
Gino Heyman <gino.heyman@gmail.com>
2023-12-03 13:54:18 +0100

    feat(day3): part 2

--- src/day3.zig
+++ src/day3.zig
@@ -3,11 +3,11 @@ const testing = std.testing;
 const Input = @import("Input.zig");
 
 test "day 2, part 1" {
-    var list = std.ArrayList([]const u8).init(testing.allocator);
-    defer list.deinit();
-
     var it = try Input.readLines("src/day3.txt", testing.allocator);
     defer it.deinit() catch unreachable;
+
+    var list = std.ArrayList([]const u8).init(testing.allocator);
+    defer list.deinit();
 
     while (it.next()) |line| {
         try list.append(line);
@@ -20,31 +20,117 @@ test "day 2, part 1" {
         var candidateStart: ?usize = null;
 
         for (0..line.len) |x| {
-            if (isDigit(line, x)) {
-                if (candidateStart == null) {
-                    candidateStart = x;
-                }
-            } else if (candidateStart != null) {
-                const v = try std.fmt.parseInt(u32, line[candidateStart.?..x], 10);
-
-                if (hasAdjacentDigits(list, y, candidateStart.?, x - 1)) {
-                    sum += v;
-                }
-
-                candidateStart = null;
+            if (isDigit(line, x) and candidateStart == null) {
+                candidateStart = x;
             }
-        }
 
-        if (candidateStart != null) {
-            const v = try std.fmt.parseInt(u32, line[candidateStart.?..line.len], 10);
+            if (candidateStart != null) {
+                if (!isDigit(line, x)) {
+                    const v = try std.fmt.parseInt(u32, line[candidateStart.?..x], 10);
 
-            if (hasAdjacentDigits(list, y, candidateStart.?, line.len - 1)) {
-                sum += v;
+                    if (hasAdjacentDigits(list, y, candidateStart.?, x - 1)) {
+                        sum += v;
+                    }
+
+                    candidateStart = null;
+                } else if (x == line.len - 1) {
+                    const v = try std.fmt.parseInt(u32, line[candidateStart.?..line.len], 10);
+
+                    if (hasAdjacentDigits(list, y, candidateStart.?, x)) {
+                        sum += v;
+                    }
+
+                    candidateStart = null;
+                }
             }
         }
     }
 
     std.debug.print("solution: {}\n", .{sum});
+    try testing.expect(sum == 525119);
+}
+
+test "day 2, part 2" {
+    var list = std.ArrayList([]const u8).init(testing.allocator);
+    defer list.deinit();
+
+    var it = try Input.readLines("src/day3.txt", testing.allocator);
+    defer it.deinit() catch unreachable;
+
+    while (it.next()) |line| {
+        try list.append(line);
+    }
+
+    var gears = std.AutoHashMap([2]usize, [2]u32).init(testing.allocator);
+    defer gears.deinit();
+
+    for (0..list.items.len) |y| {
+        const line = list.items[y];
+        var candidateStart: ?usize = null;
+
+        for (0..line.len) |x| {
+            if (isDigit(line, x) and candidateStart == null) {
+                candidateStart = x;
+            }
+
+            if (candidateStart != null) {
+                if (!isDigit(line, x)) {
+                    const v = try std.fmt.parseInt(u32, line[candidateStart.?..x], 10);
+
+                    if (hasAdjacentGear(list, y, candidateStart.?, x - 1)) |gear| {
+                        const current = gears.get(gear) orelse .{ 0, 1 };
+                        try gears.put(gear, .{ current[0] + 1, current[1] * v });
+                    }
+
+                    candidateStart = null;
+                } else if (x == line.len - 1) {
+                    const v = try std.fmt.parseInt(u32, line[candidateStart.?..line.len], 10);
+
+                    if (hasAdjacentGear(list, y, candidateStart.?, x)) |gear| {
+                        const current = gears.get(gear) orelse .{ 0, 1 };
+                        try gears.put(gear, .{ current[0] + 1, current[1] * v });
+                    }
+
+                    candidateStart = null;
+                }
+            }
+        }
+    }
+
+    var sum: u32 = 0;
+    var values = gears.iterator();
+    while (values.next()) |e| {
+        if (e.value_ptr.*[0] == 2) {
+            sum += e.value_ptr.*[1];
+        }
+    }
+
+    std.debug.print("solution: {}\n", .{sum});
+    try testing.expect(sum == 76504829);
+}
+
+fn hasAdjacentGear(map: std.ArrayList([]const u8), y: usize, x1: usize, x2: usize) ?[2]usize {
+    if (x1 > 0 and isGear(map.items[y], x1 - 1)) {
+        return .{ x1 - 1, y };
+    }
+
+    if (x2 < map.items[y].len - 1 and isGear(map.items[y], x2 + 1)) {
+        return .{ x2 + 1, y };
+    }
+
+    if (y > 0) {
+        if (containsGear(map.items[y - 1], x1, x2)) |x| {
+            return .{ x, y - 1 };
+        }
+    }
+
+    if (y < map.items.len - 1) {
+        if (containsGear(map.items[y + 1], x1, x2)) |x| {
+            return .{ x, y + 1 };
+        }
+    }
+
+    return null;
 }
 
 fn hasAdjacentDigits(map: std.ArrayList([]const u8), y: usize, x1: usize, x2: usize) bool {
@@ -56,18 +142,18 @@ fn hasAdjacentDigits(map: std.ArrayList([]const u8), y: usize, x1: usize, x2: us
         return true;
     }
 
-    if (y > 0 and containsDigits(map.items[y - 1], x1, x2)) {
+    if (y > 0 and containsSymbols(map.items[y - 1], x1, x2)) {
         return true;
     }
 
-    if (y < map.items.len - 1 and containsDigits(map.items[y + 1], x1, x2)) {
+    if (y < map.items.len - 1 and containsSymbols(map.items[y + 1], x1, x2)) {
         return true;
     }
 
     return false;
 }
 
-fn containsDigits(line: []const u8, x1: usize, x2: usize) bool {
+fn containsSymbols(line: []const u8, x1: usize, x2: usize) bool {
     const from = if (x1 == 0) 0 else x1 - 1;
     const to = @min(x2 + 1, line.len - 1);
 
@@ -78,6 +164,23 @@ fn containsDigits(line: []const u8, x1: usize, x2: usize) bool {
     }
 
     return false;
+}
+
+fn containsGear(line: []const u8, x1: usize, x2: usize) ?usize {
+    const from = if (x1 == 0) 0 else x1 - 1;
+    const to = @min(x2 + 1, line.len - 1);
+
+    for (from..to + 1) |x| {
+        if (isSymbol(line, x)) {
+            return x;
+        }
+    }
+
+    return null;
+}
+
+fn isGear(s: []const u8, i: usize) bool {
+    return s[i] == '*';
 }
 
 fn isSymbol(s: []const u8, i: usize) bool {
