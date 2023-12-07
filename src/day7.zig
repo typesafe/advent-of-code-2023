1bc4fff9cadc3a41eecd294f0fcea0b33c0277b1
Gino Heyman <gino.heyman@qover.com>
2023-12-07 22:40:28 +0100

    feat(day7): part 2 + clean up

--- src/day7.zig
+++ src/day7.zig
@@ -6,7 +6,7 @@ test "day 7, part 1" {
     var lines = try Input.readLines("src/day7.txt", testing.allocator);
     defer lines.deinit() catch unreachable;
 
-    const hands = try parseHands(lines, testing.allocator);
+    const hands = try parseHands(Hand, lines, testing.allocator);
     defer {
         for (hands) |hand| {
             var h = hand;
@@ -14,8 +14,6 @@ test "day 7, part 1" {
         }
         testing.allocator.free(hands);
     }
-
-    std.sort.insertion(Hand, hands, {}, Hand.sort);
 
     var score: usize = 0;
     for (hands, 1..) |hand, exp| {
@@ -26,42 +24,108 @@ test "day 7, part 1" {
     try testing.expect(score == 248179786);
 }
 
-fn parseHands(lines: Input.InputIterator, allocator: std.mem.Allocator) ![]Hand {
-    var hands = std.ArrayList(Hand).init(allocator);
+test "day 7, part 2" {
+    var lines = try Input.readLines("src/day7.txt", testing.allocator);
+    defer lines.deinit() catch unreachable;
 
-    while (lines.next()) |line| {
-        try hands.append(try Hand.init(line, allocator));
+    const hands = try parseHands(HandWithJokers, lines, testing.allocator);
+    defer {
+        for (hands) |hand| {
+            var h = hand;
+            h.deinit();
+        }
+        testing.allocator.free(hands);
     }
 
-    return hands.toOwnedSlice();
+    var score: usize = 0;
+    for (hands, 1..) |hand, exp| {
+        score += hand.bid * exp;
+    }
+
+    std.debug.print("solution: {}\n", .{score});
+    try testing.expect(score == 247885995);
 }
 
-const Hand = struct {
+fn parseHands(comptime T: type, lines: Input.InputIterator, allocator: std.mem.Allocator) ![]T {
+    var hands = std.ArrayList(T).init(allocator);
+
+    while (lines.next()) |line| {
+        try hands.append(try T.init(line, allocator));
+    }
+
+    const slice = try hands.toOwnedSlice();
+
+    std.sort.insertion(T, slice, {}, T.sort);
+
+    return slice;
+}
+
+const HandWithJokers = struct {
     input: []const u8,
     cards: std.AutoHashMap(usize, usize),
     strength: usize,
     bid: usize,
 
+    pub fn init(data: []const u8, allocator: std.mem.Allocator) !@This() {
+        var cards = std.AutoHashMap(usize, usize).init(allocator);
+        var strength: usize = 0;
+        var highestCount: usize = 0;
+        var jokers: usize = 0;
+
+        for (data[0..5], 0..) |card, exp| {
+            const val = cardValuesWithJoker.get(&.{card}).?;
+            strength += val * std.math.pow(usize, 15, 4 - exp);
+
+            if (val == 1) {
+                jokers += 1;
+                continue; // we use jokers later
+            }
+
+            const count = if (cards.get(val)) |count| count else 0;
+            try cards.put(val, count + 1);
+            if (count + 1 > highestCount) {
+                highestCount = count + 1;
+            }
+        }
+
+        if (jokers == 5) {
+            strength += @as(usize, 5) * std.math.pow(usize, 15, 4 + 5);
+        }
+
+        var it = cards.valueIterator();
+        while (it.next()) |count| {
+            if (count.* == highestCount and jokers > 0) {
+                const funnyCount = count.* + jokers;
+                strength += @as(usize, funnyCount) * std.math.pow(usize, 15, 4 + funnyCount);
+                jokers = 0;
+            } else if (count.* > 1) {
+                strength += @as(usize, count.*) * std.math.pow(usize, 15, 4 + count.*);
+            }
+        }
+
+        return @This(){
+            .input = data,
+            .cards = cards,
+            .strength = strength,
+            .bid = @as(usize, @bitCast(Input.parseInt(data[6..]))),
+        };
+    }
+
+    pub fn deinit(self: *@This()) void {
+        self.cards.deinit();
+    }
+
+    fn sort(_: void, a: @This(), b: @This()) bool {
+        return a.strength < b.strength;
+    }
+};
+
+const Hand = struct {
+    cards: std.AutoHashMap(usize, usize),
+    strength: usize,
+    bid: usize,
+
     pub fn init(data: []const u8, allocator: std.mem.Allocator) !Hand {
-        // var cards = std.AutoHashMap(u8, u8).init(allocator);
-
-        // for (data[0..5]) |card| {
-        //     const val = cardValues.get(&.{card}).?;
-        //     const count = if (cards.get(val)) |count| count else 0;
-        //     try cards.put(val, count + 1);
-        // }
-
-        // var strength: usize = 0;
-        // var it = cards.iterator();
-        // while (it.next()) |entry| {
-        //     const val = @as(usize, entry.key_ptr.*);
-        //     const exp = @as(usize, entry.value_ptr.* - 1);
-        //     // std.debug.print("key: {}\n", .{val});
-        //     // std.debug.print("key: {}\n", .{exp});
-
-        //     strength += val * 100 ^ exp;
-        // }
-
         var cards = std.AutoHashMap(usize, usize).init(allocator);
         var strength: usize = 0;
 
@@ -80,7 +144,6 @@ const Hand = struct {
         }
 
         return Hand{
-            .input = data,
             .cards = cards,
             .strength = strength,
             .bid = @as(usize, @bitCast(Input.parseInt(data[6..]))),
